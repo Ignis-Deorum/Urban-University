1ee499150024223914701c44df70d4dd6da0128f
Ignis-Deorum <y.s.ovsyannikov@gmail.com>
2024-08-13 05:37:27 +0300

    Variables

---
+++ Variables/task1.py
@@ -0,0 +1,5 @@
+completed_exercises = 12
+hours_spent = 1.5
+course_name = 'Python'
+time_per_exercise = hours_spent / completed_exercises
+print('Курс: ' + course_name + ', всего задач: ' + str(completed_exercises) + ', затрачено часов: ' + str(hours_spent) + ', среднее время выполнения ' + str(time_per_exercise) + ' часа')
